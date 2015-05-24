23049cd23e95d37679f19e9a871babe0878d0712
Konstantin Bukin <kbukin@nvidia.com>
2015-05-24 12:28:07 -0700

    added new mask bits for exec() and stat() events

--- include/linux/pnotify.h
+++ include/linux/pnotify.h
@@ -17,14 +17,17 @@ struct pnotify_event {
 	char		name[0];	/* Full path name */
 };
 
-#define PN_ANNOTATE		0x00100000      /* pnotify system: annotate event */
-#define PN_PROCESS_CREATE	0x00200000      /* pnotify system: an observed process was created */
-#define PN_PROCESS_EXIT 	0x00400000      /* pnotify system: an observed process exited */
+#define PN_ANNOTATE		    0x00100000  /* pnotify system: annotate event */
+#define PN_PROCESS_CREATE	0x00200000  /* pnotify system: an observed process was created */
+#define PN_PROCESS_EXIT 	0x00400000  /* pnotify system: an observed process exited */
+#define PN_EXEC_CMD       0x00800000  /* pnotify system: an observed process called exec() */
+#define PN_STAT           0x10000000  /* pnotify system: an observed process called ?stat() */
 
-#define PN_ALL_EVENTS ( IN_ALL_EVENTS | PN_ANNOTATE | PN_PROCESS_CREATE | PN_PROCESS_EXIT )
+#define PN_ALL_EVENTS (IN_ALL_EVENTS | PN_ANNOTATE | PN_PROCESS_CREATE | PN_PROCESS_EXIT | PN_EXEC_CMD | PN_STAT)
 
 #ifdef __KERNEL__
 extern struct ctl_table pnotify_table[]; /* for sysctl */
+int pnotify_broadcast_event(struct task_struct *task, u32 event_type, const char *msg);
 #endif
 
 #endif	/* _LINUX_PNOTIFY_H */
